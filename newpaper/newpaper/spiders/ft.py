c4567fe68b94bba7bf2a3a675d99b1141f55d84c
dengfan <dengfan@diandian-tech.com>
2017-11-21 13:53:50 +0800

    获取xml item 信息

--- newpaper/newpaper/spiders/ft.py
+++ newpaper/newpaper/spiders/ft.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
+import json
 from scrapy.spider import Spider
 from scrapy.selector import Selector
-from newpaper.items import NewpaperItem
-
 class FtSpider(Spider):
     name = 'ft'
     allowed_domains = ['http://www.ftchinese.com']
     start_urls = ['http://www.ftchinese.com/rss/feed']
-
     def parse(self, response):
-            select = Selector(text=response.body)
-            print select.xpath('//title/text()').extract()
+            select = Selector(text=response.body,type='xml')
+            itemlist = select.xpath('//item')
+            for index,item in enumerate(itemlist):
+                    des = item.xpath('./description/text()').extract()    
+                    title = item.xpath('./title/text()').extract()    
+                    link = item.xpath('./link/text()').extract()    
+                    time = item.xpath('./pubDate/text()').extract()
+                    print index,title[0].encode('utf-8'),link[0].encode('utf-8'),time[0].encode('utf-8') 
             pass
